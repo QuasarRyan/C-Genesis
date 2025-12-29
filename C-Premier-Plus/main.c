7838d7c13cbfc61fca7debe5d5c765a0b24fb270
QuasarRyan <lewis04023@icloud.com>
2025-12-29 20:53:58 +0800

    Initial Commit

---
+++ C-Premier-Plus/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-Premier-Plus
+//
+//  Created by QuasarRyan on 2025/12/29.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
